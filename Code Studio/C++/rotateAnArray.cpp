8d5725584abd1b520f9e0e089d11a73bb622caad
sourabh kushwah <uuc110@gmail.com>
2022-11-11 21:13:27 +0530

    File Refactor

--- Code Studio/C++/rotateAnArray.cpp
+++ Code Studio/C++/rotateAnArray.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 #define RAND rand() % 11;
 
-void Rotateanarray(int* arr, int n, int d){
+void Rotateanarray(int *arr, int n, int d) {
     int temp[d];
-    for(int i = 0; i < d; i++){
+    for (int i = 0; i < d; i++) {
         temp[i] = arr[i];
     }
-    for(int i = 0; i < n-d; i++){
-        arr[i] = arr[i+d];
+    for (int i = 0; i < n - d; i++) {
+        arr[i] = arr[i + d];
     }
-    for(int i = 0; i < d; i++){
-        arr[n-d+i] = temp[i];
+    for (int i = 0; i < d; i++) {
+        arr[n - d + i] = temp[i];
     }
 
 }
@@ -24,19 +24,19 @@ int main() {
     freopen("output.txt", "w", stdout);
     srand(time(NULL));
     int t = rand() % 10;
-    cout<< "======================================" << endl;
-    cout << "            Test Case: "<< t << "           "<<  endl;
-    cout<< "======================================" << endl;
-    while(t--){
+    cout << "======================================" << endl;
+    cout << "            Test Case: " << t << "           " << endl;
+    cout << "======================================" << endl;
+    while (t--) {
         int n = rand() % 11;
-        cout << "Number of Element: " <<  n << endl;
+        cout << "Number of Element: " << n << endl;
         int d = rand() % n;
         cout << "Rotation: " << d << endl;
         cout << "Before Rotated Araray: " << endl;
         int arr[n];
-        for(int i=0; i< n;i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = rand() % 11;
-            cout<< arr[i] << " ";
+            cout << arr[i] << " ";
         }
         cout << endl;
 
@@ -44,11 +44,11 @@ int main() {
         Rotateanarray(arr, n, d);
 
         cout << "After Rotated Araray: " << endl;
-        for(int i=0; i< n;i++){
+        for (int i = 0; i < n; i++) {
             cout << arr[i] << " ";
         }
         cout << endl;
-        cout<< "======================================" << endl;
+        cout << "======================================" << endl;
 
     }
 
